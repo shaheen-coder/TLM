e89b3ce5398a2cdd5cadc366eb2943391471c1cc
shaheen-coder <shaheenvsa@gmail.com>
2026-03-17 14:52:49 +0530

    [IMPR] fixed loss pad tok and logits scale issue

--- train/trainer.py
+++ train/trainer.py
@@ -40,10 +40,38 @@ config = ModelConfig()
 
 model = TinyLM(config)
 
+# fix loss fn to ingore pad tokens
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction="none"
+)
+
+
+def masked_pad_loss(y_true, y_pred):
+
+    loss = loss_fn(y_true, y_pred)
+
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+
+    loss = loss * mask
+    denom = tf.reduce_sum(mask)
+    return tf.reduce_sum(loss) / tf.maximum(denom, 1.0)
+
+
+# kears cant ingore the pad token
+def masked_accuracy(y_true, y_pred):
+    preds = tf.argmax(y_pred, axis=-1, output_type=tf.int32)
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+
+    match = tf.cast(tf.equal(y_true, preds), tf.float32)
+    match *= mask
+
+    return tf.reduce_sum(match) / tf.maximum(tf.reduce_sum(mask), 1.0)
+
+
 model.compile(
     optimizer=tf.keras.optimizers.Adam(1e-4),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=["accuracy"],
+    loss=masked_pad_loss,
+    metrics=masked_accuracy,
 )
 
 model.fit(trainer_dataset, epochs=2)
