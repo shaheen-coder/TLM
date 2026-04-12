d831e14432b190890bc7bff0f53a02167ab8d878
shaheen-coder <shaheenvsa@gmail.com>
2026-04-12 17:37:39 +0530

    [UPD] [TEST] new model arch

--- model/transformer.py
+++ model/transformer.py
@@ -1,5 +1,5 @@
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Embedding, LSTM, Dropout
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Attention
 from tensorflow.keras.saving import (
     register_keras_serializable,
     serialize_keras_object,
@@ -9,6 +9,8 @@ import tensorflow as tf
 
 # custom imoprt
 from model.config import ModelConfig
+
+from typing import Tuple
 
 
 @register_keras_serializable(package="tinylm")
@@ -24,12 +26,11 @@ class TinyLM(Model):
             embeddings_initializer="glorot_uniform",
             name="shared_embedding",
         )
-        self.dropout = Dropout(config.dropout)
         # -------------  encoder -------------
         self.encoder_lstm = LSTM(
             units=config.d_model,
             return_state=True,
-            return_sequences=False,
+            return_sequences=True,
             dropout=config.dropout,
             name="encoder_lstm",
         )
@@ -40,6 +41,15 @@ class TinyLM(Model):
             return_sequences=True,
             dropout=config.dropout,
             name="decoder_lstm",
+        )
+        # --------------- Attention --------
+        self.attention = Attention(
+            use_scale=False,
+            name="luong_attention",
+        )
+        # -------------- Attention proj ----
+        self.atn_proj = Dense(
+            units=config.d_model, activation="tanh", name="attn_projection"
         )
 
         # ------------- FF -------------
@@ -63,29 +73,36 @@ class TinyLM(Model):
 
     def encoder(
         self, encoder_input: tf.Tensor, training: bool = False
-    ) -> list[tf.Tensor, tf.Tensor]:
+    ) -> Tuple[tf.Tensor, list[tf.Tensor]]:
         x = self.embedding(encoder_input)
-        x = self.dropout(x, training=training)
 
         mask = self.embedding.compute_mask(encoder_input)
 
-        _, h, c = self.encoder_lstm(x, mask=mask, training=training)
+        encoder_outputs, h, c = self.encoder_lstm(x, mask=mask, training=training)
 
-        return [h, c]
+        return encoder_outputs, [h, c]
 
-    def decoder(self, decoder_inputs: tf.Tensor, states, training: bool = False):
+    def decoder(
+        self,
+        encoder_outputs: tf.Tensor,
+        decoder_inputs: tf.Tensor,
+        states,
+        training: bool = False,
+    ):
         y = self.embedding(decoder_inputs)
-        y = self.dropout(y, training=training)
 
         mask = self.embedding.compute_mask(decoder_inputs)
 
         out, h, c = self.decoder_lstm(
             y, mask=mask, initial_state=states, training=training
         )
-        # out *= tf.math.sqrt(
-        #    tf.cast(self.config.d_model, tf.float32)
-        # )  # scale by sqrt of 512
-        logits = out @ tf.transpose(self.embedding.embeddings) + self.logits_bias
+        context = self.attention([out, encoder_outputs], training=training)
+        combined = tf.concat([context, out], axis=-1)
+        attn_hidden = self.atn_proj(combined)
+
+        logits = (
+            attn_hidden @ tf.transpose(self.embedding.embeddings) + self.logits_bias
+        )
 
         return logits, [h, c]
 
@@ -94,8 +111,10 @@ class TinyLM(Model):
         encoder_input = inputs[0]
         decoder_input = inputs[1]
         # ------------- Encoder Part -------------
-        encoder_state = self.encoder(encoder_input, training=training)
+        encoder_outputs, encoder_state = self.encoder(encoder_input, training=training)
         # ------------- Decoder Part -------------
-        logits, _ = self.decoder(decoder_input, encoder_state, training=training)
+        logits, _ = self.decoder(
+            encoder_outputs, decoder_input, encoder_state, training=training
+        )
 
         return logits
