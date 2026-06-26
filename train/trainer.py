b368550d1a7b5f870f40ff94cd6104c3cc2d47ce
shaheen-coder <shaheenvsa@gmail.com>
2026-06-26 12:08:08 +0530

    [FIX] optimized tf trainer pipeline

--- train/trainer.py
+++ train/trainer.py
@@ -1,3 +1,4 @@
+import math
 import os
 from datetime import datetime
 import numpy as np
@@ -7,12 +8,14 @@ from model.transformer import TinyLM
 from model.config import ModelConfig
 
 # --- Configuration ---
-BATCH_SIZE = 18
+BATCH_SIZE = 32
+FT_EPOCH : int = 10
+
 log_dir = os.path.join("logs", datetime.now().strftime("%Y%m%d-%H%M%S"))
 tb_cb = tf.keras.callbacks.TensorBoard(
     log_dir=log_dir,
-    histogram_freq=1,
-    write_graph=True,
+    histogram_freq=0,
+    write_graph=False,
     update_freq="epoch",
 )
 
@@ -29,7 +32,7 @@ ft_target_arr = np.load("datasets/pretokens/ft_target.npy", mmap_mode="r")
 
 
 ft_dataset = tf.data.Dataset.from_tensor_slices((ft_input_arr, ft_target_arr))
-ft_dataset = ft_dataset.shuffle(10000).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+ft_dataset = ft_dataset.shuffle(10000,reshuffle_each_iteration=True).batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 
 # Validation
 val_input_arr = np.load("datasets/pretokens/ft_val_input.npy", mmap_mode="r")
@@ -38,12 +41,23 @@ val_target_arr = np.load("datasets/pretokens/ft_val_target.npy", mmap_mode="r")
 val_dataset = tf.data.Dataset.from_tensor_slices((val_input_arr, val_target_arr))
 val_dataset = val_dataset.batch(BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
 
+# --- Cosine Scheduler --
+steps_per_epoch = math.ceil(len(ft_input_arr) // BATCH_SIZE)
+total_steps = steps_per_epoch * FT_EPOCH
+
+lr_schedule = tf.keras.optimizers.schedules.CosineDecay(
+    initial_learning_rate=1e-4,
+    decay_steps=total_steps,
+    alpha=0.1
+)
 
 # --- Custom Loss & Metrics ---
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True, reduction=tf.keras.losses.Reduction.NONE
+)
+
 def masked_pad_loss(y_true, y_pred):
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction=tf.keras.losses.Reduction.NONE
-    )
     loss = loss_fn(y_true, y_pred)
 
     mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
@@ -67,13 +81,15 @@ config = ModelConfig()
 model = TinyLM(config)
 
 optimizer = tf.keras.optimizers.AdamW(
-    learning_rate=1e-4, weight_decay=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-7
+    learning_rate=lr_schedule, weight_decay=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-7
 )
 
 model.compile(
     optimizer=optimizer,
     loss=masked_pad_loss,
     metrics=[masked_accuracy],
+    jit_compile=True,
+    steps_per_execution=32
 )
 
 # --- Training ---
@@ -83,8 +99,6 @@ model.compile(
     # epochs=FD_EPOCH,
     # callbacks=[tb_cb]
 # )
-FT_EPOCH : int = 2
-optimizer.learning_rate.assign(2e-5)
 model.fit(
     ft_dataset,
     validation_data=val_dataset,
